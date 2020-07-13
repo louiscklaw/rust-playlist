e8655c944e33039d895406702cc0bee4cc157d94
louiscklaw <louiscklaw@gmail.com>
2020-07-13 18:23:54 +0800

    update,

---
+++ hello-cargo/target/debug/deps/hello_world-dcf7c9a2b199bc7a.d
@@ -0,0 +1,5 @@
+/home/logic/_workspace/rust-playlist/hello-cargo/target/debug/deps/hello_world-dcf7c9a2b199bc7a: src/main.rs
+
+/home/logic/_workspace/rust-playlist/hello-cargo/target/debug/deps/hello_world-dcf7c9a2b199bc7a.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/logic/_workspace/rust-playlist/hello-cargo/target/debug/hello_world: /home/logic/_workspace/rust-playlist/hello-cargo/src/main.rs
